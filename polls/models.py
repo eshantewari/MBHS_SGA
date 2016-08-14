38bc7eb38d61d8c13bd97f64f988d118519c923a
root <eshan.tewari@gmail.com>
2016-08-13 21:20:48 -0400

    Some Bug Fixes

--- polls/models.py
+++ polls/models.py
@@ -4,13 +4,13 @@ from django.utils import timezone
 from django.template.defaultfilters import slugify
 
 
-#The Class which accesses/manages the database of students/login info
+#The Class which keeps a record of student records
 class Students(models.Model):
     student_id = models.IntegerField(primary_key=True)
     grade = models.IntegerField() #this will be deleted after PAM
     password = models.CharField(max_length=30, blank=True, null=True) #this will be deleted after PAM
     #This will be a huge-ass string used solely for authentication/record purposes: it will just be a concatenated list of all of the student's votes
-    votes = models.TextField(blank = True, null = True) 
+    votes = models.TextField(blank = True, null = True)
     class Meta:
         verbose_name_plural = "Students"
 
@@ -18,8 +18,8 @@ class Category(models.Model):
     category_text = models.CharField(max_length = 200)
     category_num = models.IntegerField(default = 0)
     pub_date = models.DateTimeField('date published')
-    grade_level = models.IntegerField(default = 0)    
-    
+    grade_level = models.IntegerField(default = 0)
+
     #I used slugs as an alternative for primary keys because:
     #1) primary keys can get nasty and become anachronistic when data is edited
     #2) it's easier to keep track of slugs
@@ -27,7 +27,7 @@ class Category(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.category_num)
         super(Category, self).save(*args, **kwargs)
-        
+
     class Meta:
         verbose_name_plural = "Categories"
 
--- polls/views.py
+++ polls/views.py
@@ -10,16 +10,18 @@ from django import forms
 from .forms import LoginForm
 from .models import Candidate, Category, Students
 
-user_grade = 0
-student_id = 0
-votes = []
 
-
+#Render the Home Page
 def index(request):
     categories = Category.objects.order_by("-category_num")
     context = {'category_list': categories}
     return render(request, 'polls/index.html',context)
 
+#Authenticate the login
+#Step 1: Authenticate with PAM backend
+#Step 2: Ensure that the user has not voted
+#Step 3: Access Grade Level via ....
+#Step 4: Create session variables for user_grade, student_id, and votes array
 def authenticate(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -31,12 +33,11 @@ def authenticate(request):
                 #entries  = Students.objects.filter(student_id=username)
                 #if entries:
                     #return HttpResponseRedirect(reverse('polls:already_voted'))
-                #global student_id
-                #student_id = username
+                    #request.session['student_id'] = username
                 #user_grade???
             entries  = Students.objects.filter(student_id=form.cleaned_data.get('username'))
-            global user_grade
-            user_grade = entries[0].grade
+            request.session['user_grade'] = entries[0].grade
+            request.session['votes'] = []
             return HttpResponseRedirect(reverse('polls:detail',args=(1,))) #Go to the first voting page
     else:
         form = LoginForm()
@@ -45,10 +46,10 @@ def authenticate(request):
 
 #The View for the Voting Pages - this will display candidates along with their mottos in a pseudo-random order
 @login_required
-def detail(request, slug):
+def detail(request, category_id):
     category = get_object_or_404(Category, slug=slug)
-    global user_grade
-    if category.grade_level == user_grade or category.grade_level == 0:
+    #A grade_level of 0 indicates that it applies to all grades
+    if category.grade_level == request.session['user_grade'] or category.grade_level == 0:
         #Randomizing
         candidate_list = category.candidate_set.all()
         candidates = []
@@ -58,7 +59,7 @@ def detail(request, slug):
         random.shuffle(candidates)
         candidates.append(candidate_list[len(candidate_list)-1])
         #Render up the detail page with the randomized list
-        return render(request, 'polls/detail.html', {'candidates': candidates,'category':category}) 
+        return render(request, 'polls/detail.html', {'candidates': candidates,'category':category})
     elif category.category_num + 1 >= Category.objects.count():
         return HttpResponseRedirect(reverse('polls:thanks'))
     else:
@@ -76,22 +77,21 @@ def vote(request, slug):
             'error_message': "You didn't select a choice.",
         })
     else:
-        global votes
+        votes = request.session['votes']
         votes.append(selected_candidate.candidate_name)
+        request.session['votes'] = votes
         selected_candidate.votes += 1
         selected_candidate.save()
     if p.category_num >= Category.objects.count():
             return HttpResponseRedirect(reverse('polls:thanks'))
     else:
         return HttpResponseRedirect(reverse('polls:detail',args=(p.category_num+1,)))
-    
-@login_required    
+
+@login_required
 def thanks(request):
     #student = Student()
-    #global student_id
-    #student.username = student_id
-    #global votes
-    #votes_str = ' '.join(votes)
+    #student.username = request.session['student_id']
+    #votes_str = ' '.join(request.session['votes'])
     #student.votes = votes_str
     #student.save()
     #logout(request)
